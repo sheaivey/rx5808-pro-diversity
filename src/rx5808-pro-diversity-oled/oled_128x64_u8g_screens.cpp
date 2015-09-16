38c93de680cc4f6f325fef61f86d7587df42b49b
sheaivey <ivey.shea@gmail.com>
2015-09-15 22:40:01 -0700

    adding header to u8glib

--- src/rx5808-pro-diversity-oled/oled_128x64_u8g_screens.cpp
+++ src/rx5808-pro-diversity-oled/oled_128x64_u8g_screens.cpp
@@ -1,4 +1,31 @@
 /*
+ * u8glib screens by Shea Ivey
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Shea Ivey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+/*
     The u8glib screens were started and later abandoned due to performance issues.
     Here for reference should anyone want to take on this task.
 
@@ -92,6 +119,7 @@ void screens::updateSeekMode(uint8_t state, uint8_t channelIndex, uint8_t channe
 
 void screens::bandScanMode(uint8_t state) {
     reset(); // start from fresh screen.
+    best_rssi = 0;
     if(state==STATE_SCAN)
     {
         drawTitleBox("BAND SCANNER");
@@ -103,7 +131,8 @@ void screens::bandScanMode(uint8_t state) {
 }
 
 void screens::updateBandScanMode(bool in_setup, uint8_t channel, uint8_t rssi, uint8_t channelName, uint16_t channelFrequency, uint16_t rssi_setup_min_a, uint16_t rssi_setup_max_a) {
-
+    static uint8_t writePos=SCANNER_LIST_X_POS;
+    last_channel = channel;
 }
 
 void screens::screenSaver(uint8_t channelName, uint16_t channelFrequency) {
