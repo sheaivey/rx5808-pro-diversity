511b4a75b9727db30d73a22a5b42a3553d6a4212
Daniel Callander <knifacat@gmail.com>
2017-02-19 14:27:52 +0000

    Drop boot check/screen, disable serial by default

--- src/rx5808-pro-diversity/settings.h
+++ src/rx5808-pro-diversity/settings.h
@@ -66,8 +66,7 @@ SOFTWARE.
 
 #define USE_DIVERSITY
 //#define USE_IR_EMITTER
-//#define USE_BOOT_LOGO
-#define USE_SERIAL_OUT // Not compatible with IR emitter.
+//#define USE_SERIAL_OUT // Not compatible with IR emitter.
 
 // You can use any of the arduino analog pins to measure the voltage of the
 // battery. See additional configuration below.
@@ -84,8 +83,6 @@ SOFTWARE.
 //     5621 MHz
 // Local laws may prohibit the use of these frequencies so use at your own risk!
 #define USE_LBAND
-//#define USE_BOOT_CHECK
-#define USE_DIM_ON_SCREENSAVER
 
 // === Pins ====================================================================
 
