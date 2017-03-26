f1d913a3b3e2088b94750f89b09a10af8a7c05b2
Daniel Callander <knifacat@gmail.com>
2017-03-27 00:15:12 +0100

    Improve look of band scanner

--- src/rx5808-pro-diversity/state_bandscan.cpp
+++ src/rx5808-pro-diversity/state_bandscan.cpp
@@ -36,57 +36,137 @@ void StateMachine::BandScanStateHandler::onUpdate() {
     Receiver::setChannel(Channels::getOrderedIndex(orderedChanelIndex));
 
     Ui::needUpdate();
+
+    if (orderedChanelIndex == 0) {
+
+    }
 }
+
+#define BORDER_LEFT_X 0
+#define BORDER_LEFT_Y 0
+#define BORDER_LEFT_H (SCREEN_HEIGHT - CHAR_HEIGHT - 1)
+
+#define BORDER_RIGHT_X (SCREEN_WIDTH - 1)
+#define BORDER_RIGHT_Y 0
+#define BORDER_RIGHT_H BORDER_LEFT_H
+
+#define BORDER_BOTTOM_X 0
+#define BORDER_BOTTOM_Y (SCREEN_HEIGHT - CHAR_HEIGHT - 2)
+#define BORDER_BOTTOM_W SCREEN_WIDTH
+
+#define CHANNEL_TEXT_LOW_X 0
+#define CHANNEL_TEXT_LOW_Y (SCREEN_HEIGHT - CHAR_HEIGHT)
+#define CHANNEL_TEXT_W ((CHAR_WIDTH + 1) * 4)
+
+#define CHANNEL_TEXT_HIGH_X (SCREEN_WIDTH - CHANNEL_TEXT_W + 1)
+#define CHANNEL_TEXT_HIGH_Y CHANNEL_TEXT_LOW_Y
+
+#define BORDER_PROGRESS_LEFT_X (CHANNEL_TEXT_LOW_X + CHANNEL_TEXT_W)
+#define BORDER_PROGRESS_RIGHT_X (CHANNEL_TEXT_HIGH_X - 2)
+#define BORDER_PROGRESS_Y BORDER_BOTTOM_Y
+#define BORDER_PROGRESS_H SCREEN_HEIGHT - BORDER_BOTTOM_Y
+
+#define PROGRESS_X (BORDER_PROGRESS_LEFT_X + 2)
+#define PROGRESS_Y (BORDER_PROGRESS_Y + 2)
+#define PROGRESS_W (BORDER_PROGRESS_RIGHT_X - PROGRESS_X - 1)
+#define PROGRESS_H (SCREEN_HEIGHT - PROGRESS_Y) - 2
+
+#define GRAPH_X (BORDER_LEFT_X + 1)
+#define GRAPH_Y 0
+#define GRAPH_W (BORDER_RIGHT_X - GRAPH_X)
+#define GRAPH_H BORDER_BOTTOM_Y
 
 
 void StateMachine::BandScanStateHandler::onInitialDraw() {
-
-}
-
-void StateMachine::BandScanStateHandler::onUpdateDraw() {
     Ui::clear();
 
-    Ui::drawGraph(
-        rssiData,
-        CHANNELS_SIZE,
-        100,
-        1,
-        0,
-        SCREEN_WIDTH - 3,
-        SCREEN_HEIGHT - 1
-    );
-
-    Ui::drawDashedVLine(
-        orderedChanelIndex * (SCREEN_WIDTH / CHANNELS_SIZE),
-        0,
-        SCREEN_HEIGHT - 1,
-        4
-    );
-
     Ui::display.drawFastVLine(
-        0,
-        0,
-        SCREEN_HEIGHT - 1,
+        BORDER_LEFT_X,
+        BORDER_LEFT_Y,
+        BORDER_LEFT_H,
         WHITE
     );
 
     Ui::display.drawFastVLine(
-        SCREEN_WIDTH - 1,
-        0,
+        BORDER_RIGHT_X,
+        BORDER_RIGHT_Y,
+        BORDER_RIGHT_H,
+        WHITE
+    );
+
+    Ui::display.drawFastHLine(
+        BORDER_BOTTOM_X,
+        BORDER_BOTTOM_Y,
+        BORDER_BOTTOM_W,
+        WHITE
+    );
+
+    Ui::display.drawFastHLine(
+        BORDER_PROGRESS_LEFT_X,
         SCREEN_HEIGHT - 1,
+        BORDER_PROGRESS_RIGHT_X - BORDER_PROGRESS_LEFT_X,
+        WHITE
+    );
+
+    Ui::display.drawFastVLine(
+        BORDER_PROGRESS_LEFT_X,
+        BORDER_PROGRESS_Y,
+        BORDER_PROGRESS_H,
+        WHITE
+    );
+
+    Ui::display.drawFastVLine(
+        BORDER_PROGRESS_RIGHT_X,
+        BORDER_PROGRESS_Y,
+        BORDER_PROGRESS_H,
         WHITE
     );
 
     Ui::display.setTextSize(1);
-    Ui::display.setTextColor(INVERSE);
-    Ui::display.setCursor(2, SCREEN_HEIGHT - CHAR_HEIGHT - 2);
+    Ui::display.setTextColor(WHITE);
+    Ui::display.setCursor(CHANNEL_TEXT_LOW_X, CHANNEL_TEXT_LOW_Y);
     Ui::display.print(Channels::getFrequency(Channels::getOrderedIndex(0)));
 
-    Ui::display.setCursor(
-        SCREEN_WIDTH - 2 - (CHAR_WIDTH + 1) * 4,
-        SCREEN_HEIGHT - CHAR_HEIGHT - 2);
+    Ui::display.setCursor(CHANNEL_TEXT_HIGH_X, CHANNEL_TEXT_HIGH_Y);
     Ui::display.print(
         Channels::getFrequency(Channels::getOrderedIndex(CHANNELS_SIZE - 1)));
+
+    Ui::needDisplay();
+}
+
+void StateMachine::BandScanStateHandler::onUpdateDraw() {
+    Ui::drawGraph(
+        rssiData,
+        CHANNELS_SIZE,
+        100,
+        GRAPH_X,
+        GRAPH_Y,
+        GRAPH_W,
+        GRAPH_H
+    );
+
+    Ui::display.drawFastHLine(
+        BORDER_BOTTOM_X,
+        BORDER_BOTTOM_Y,
+        BORDER_BOTTOM_W,
+        WHITE
+    );
+
+    Ui::clearRect(
+        PROGRESS_X,
+        PROGRESS_Y,
+        PROGRESS_W,
+        PROGRESS_H
+    );
+
+    uint8_t progressW = orderedChanelIndex * PROGRESS_W / CHANNELS_SIZE + 1;
+    Ui::display.fillRect(
+        PROGRESS_X,
+        PROGRESS_Y,
+        progressW,
+        PROGRESS_H,
+        WHITE
+    );
 
     Ui::needDisplay();
 }
